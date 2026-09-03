2e280e6c6095b4082279af0506ee2fc6000921e2
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-04 00:43:01 +0530

    feat: add NavPart2 component with layout and structure

---
+++ src/components/NavPart2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavPart2() {
+  return (
+    <>
+        <div className='flex gap-5 items-center'>
+          <h5 className='text-x'>about</h5>
+          <h5 className='text-x'>contact</h5>
+          <h5 className='text-x'>services</h5>
+          <h5 className='text-x'>owner</h5>
+        </div>
+    </>
+  )
+}
+
+export default NavPart2
