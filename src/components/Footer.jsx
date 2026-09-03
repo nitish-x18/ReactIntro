169621cb29ba4cf61ba576feb2f35cdcd00478c2
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-04 00:43:20 +0530

    feat: add Footer component with layout and styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-emerald-950 py-5 w-full absolute bottom-0 flex items-center justify-center font-bold'>@nitish__x18</div>
+  )
+}
+
+export default Footer
