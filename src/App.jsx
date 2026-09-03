69dbaa400309ff60d9bcc18dd3ef22d61e20ea19
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-03 18:56:45 +0530

    Implement increment and decrement functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,19 @@ const App = () => {
   //   </div>
   // )
 
-  
+  const [a, setA] = useState(0);
+
+  const increment = ()=>{
+    setA(a+10)
+  }
+
+  return (
+    <div>
+      <h3>Number is: {a}</h3>
+      <button onClick={increment}>Increment</button>
+      <button onClick={()=>setA(a-10)}>Decrement</button>
+    </div>
+  )
 }
 
 export default App
