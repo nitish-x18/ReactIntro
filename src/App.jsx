d6c0768f58b7dc2c9a9884ceb99a14a06ab09332
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-03 18:56:14 +0530

    Refactor App component: Comment out unused code for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,31 @@ const App = () => {
   // let a = 1;
   // let User = "Nitish"
 
-  const print = ()=>{
-    console.log("Hello")
-  }
+  // const print = ()=>{
+  //   console.log("Hello")
+  // }
 
-  const [a, setA] = useState(10)
-  const changeNum = ()=>{
-    setA(20)
-  }
+  // useState HOOK----->>>
+  // const [a, setA] = useState(10)
+  // const changeNum = ()=>{
+  //   setA(20)
+  // }
 
-  const [user, setuser] = useState("Nitish")
-  const changeUser = ()=>{
-    setuser("Change ho gya")
-  }
+  // const [user, setuser] = useState("Nitish")
+  // const changeUser = ()=>{
+  //   setuser("Change ho gya")
+  // }
 
-  return (
-    <div>
-      <h2>Hello From React User: {user}, {a}</h2>
-      {/* <button onClick={print}>Click Me</button> */}
-      {/* <button onClick={changeNum}>Click Me</button> */}
-      <button onClick={changeUser}>Click Me</button>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <h2>Hello From React User: {user}, {a}</h2>
+  //     {/* <button onClick={print}>Click Me</button> */}
+  //     {/* <button onClick={changeNum}>Click Me</button> */}
+  //     <button onClick={changeUser}>Click Me</button>
+  //   </div>
+  // )
+
+  
 }
 
 export default App
