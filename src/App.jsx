47f8e50cd692bc2a0a6d3c0c88b1dc6ad1e76d78
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-03 18:42:31 +0530

    Add initial project setup with React, Vite, and SVG icons
    - Created main application file (App.jsx) with basic structure.
    - Added CSS file (index.css) for global styles.
    - Configured Vite for React support (vite.config.js).
    - Included SVG icons for various platforms in icons.svg.
    - Added hero image (hero.png) and React logo (react.svg) assets.
    - Implemented basic button functionality in App component.

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  // let a = 1;
+  // let User = "Nitish"
+
+  // const print = ()=>{
+  //   console.log("Hello")
+  // }
+
+  // const [a, setA] = useState(10)
+  // const changeNum = ()=>{
+  //   setA(20)
+  // }
+
+  // const [user, setuser] = useState("Nitish")
+  // const changeUser = ()=>{
+  //   setuser("Change ho gya")
+  // }
+
+  // return (
+  //   <div>
+  //     <h2>Hello From React User: {user}, {a}</h2>
+  //     {/* <button onClick={print}>Click Me</button> */}
+  //     {/* <button onClick={changeNum}>Click Me</button> */}
+  //     <button onClick={changeUser}>Click Me</button>
+  //   </div>
+  // )
+}
+
+export default App
