1d07221075da451725d0bce8c4a5ed45e0abc532
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-04 15:10:49 +0530

    refactor: comment out React Router DOM implementation in App component

--- src/App.jsx
+++ src/App.jsx
@@ -195,17 +195,17 @@ const App = () => {
   //   </>
   // )
 
-  //REACT ROUTER DOM--->>>
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact />}/>
-      </Routes>
-    </div>
-  )
+  // //REACT ROUTER DOM--->>>
+  // return (
+  //   <div>
+  //     <Header />
+  //     <Routes>
+  //       <Route path='/' element={<Home />}/>
+  //       <Route path='/about' element={<About />}/>
+  //       <Route path='/contact' element={<Contact />}/>
+  //     </Routes>
+  //   </div>
+  // )
 
 }
 
