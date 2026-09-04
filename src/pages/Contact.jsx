96d9a48121fed42a0b8039e6acf4f7cd53b76f83
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-04 15:09:37 +0530

    feat: add About, Contact, and Home components

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function contact() {
+  return (
+    <div>contact Page</div>
+  )
+}
+
+export default contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='font-bold text-5xl flex items-center justify-center mt-10'>Home Page</div>
+  )
+}
+
+export default Home
