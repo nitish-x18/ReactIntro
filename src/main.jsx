69365ba2b4c83e8f8f9711c214a4810434237985
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-12 17:05:34 +0530

    refactor: replace BrowserRouter with UserContext for state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "../src/index.css"
 import { BrowserRouter } from 'react-router-dom'
+import UserContext from './Context/userContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    // <BrowserRouter>
+    //     <App />
+    // </BrowserRouter>
+    <UserContext>
         <App />
-    </BrowserRouter>
+    </UserContext>
 )
